f670a264265d8284beb63ec9725cefa55f88aa70
victordscabral <victorcabral2501@gmail.com>
2023-03-20 07:21:23 -0300

    Removendo moodledata

--- moodledata/localcache/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ********** This file is generated DO NOT EDIT **********
-$CFG->siteidentifier = 'sUcDT6sSpbeCgKWr8d0W6UMK0NguOcWklocalhost';
-$CFG->bootstraphash = '1893f30be8a2070e267be6c7d1e7cc33';
-// Only if the file is not stale and has not been defined.
-if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
-    define('SYSCONTEXTID', 1);
-}
